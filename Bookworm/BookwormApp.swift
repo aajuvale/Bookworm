60342df6f6875022b1924a366d8085ac7db35d7f
aajuvale <ahmed.juvale@gmail.com>
2025-08-07 16:35:23 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ahmed Juvale on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
