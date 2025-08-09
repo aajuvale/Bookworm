3a216508d626eec015b8c4ec60d1213f696b4019
aajuvale <ahmed.juvale@gmail.com>
2025-08-09 16:37:31 -0700

    feat: added book details

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Ahmed Juvale on 8/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DetailView: View {
+    let book: Book
+
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+
+
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+
+            Text(book.review)
+                .padding()
+
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+    }
+}
