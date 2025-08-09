2f28b54b6874ceafe0a3931206b6360c13e1d97c
aajuvale <ahmed.juvale@gmail.com>
2025-08-09 16:54:55 -0700

    feat: implemented alert to pop navigationlink programmatically

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -9,6 +9,15 @@ import SwiftUI
 import SwiftData
 
 struct DetailView: View {
+    @Environment(\.modelContext)
+    var modelContext
+
+    @Environment(\.dismiss)
+    var dismiss
+
+    @State
+    private var showingDeleteAlert = false
+
     let book: Book
 
     var body: some View {
@@ -39,5 +48,23 @@ struct DetailView: View {
                 .font(.largeTitle)
         }
         .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Delete book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button("Delete this book", systemImage: "trash") {
+                showingDeleteAlert = true
+            }
+        }
+    }
+
+    func deleteBook() {
+        modelContext.delete(book)
+        dismiss()
     }
 }
